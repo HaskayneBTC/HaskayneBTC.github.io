bc74787bde9da21e1e5114663faf44e1d1091865
R Lee <alpha.r.lee.dev@gmail.com>
2019-09-09 14:27:29 -0600

    Updated display code for sponsors.

--- src/components/sponsor.jsx
+++ src/components/sponsor.jsx
@@ -3,23 +3,24 @@ import React, { Component } from "react";
 class Sponsor extends Component {
 	state = {};
 	render() {
-		const { sponsor } = this.props;
+		const { name, img, link, imgHeight } = this.props;
 
-		let imgStyle = {
-			height: this.props.height ? this.props.height : 100,
-			width: "auto"
-		};
-		if (this.props.width) {
-			imgStyle["width"] = this.props.width;
-		}
+		// let imgStyle = {
+		// 	height: this.props.height ? this.props.height : 100,
+		// 	width: "auto"
+		// };
+		// if (this.props.width) {
+		// 	imgStyle["width"] = this.props.width;
+		// }
 
 		return (
 			<div className="m-4 d-inline">
-				<a href={sponsor.link}>
+				<a href={link}>
 					<img
-						src={sponsor.img}
-						alt={sponsor.name}
-						style={imgStyle}
+						src={img}
+						alt={name}
+						className="sponsor-img"
+						style={{ height: imgHeight }}
 					/>
 				</a>
 			</div>
--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -4,27 +4,36 @@ import SectionTitle from "./utils/sectionTitle";
 
 class Sponsors extends Component {
 	state = {
-		sponsors: [
+		sponsorLevels: [
 			{
-				name: "Schulich School of Engineering",
-				img:
-					"https://upload.wikimedia.org/wikipedia/en/3/3f/Schulich.png",
-				link: "http://schulich.ucalgary.ca",
-				tier: "gold"
+				level: "Gold",
+				imgHeight: 150,
+				sponsors: [
+					{
+						name: "Haskayne School of Business",
+						img:
+							"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Haskayne_School_of_Business.svg/375px-Haskayne_School_of_Business.svg.png",
+						link: "http://haskayne.ucalgary.ca"
+					}
+				]
 			},
 			{
-				name: "Haskayne School of Business",
-				img:
-					"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Haskayne_School_of_Business.svg/375px-Haskayne_School_of_Business.svg.png",
-				link: "http://haskayne.ucalgary.ca",
-				tier: "silver"
-			},
-			{
-				name: "University of Calgary",
-				img:
-					"https://symbiont2018.files.wordpress.com/2018/10/university-of-calgary-logo-transparent-uc-horz-rgb-11.png?w=1400",
-				link: "https://www.ucalgary.ca/",
-				tier: "silver"
+				level: "Silver",
+				imgHeight: 100,
+				sponsors: [
+					{
+						name: "Schulich School of Engineering",
+						img:
+							"https://upload.wikimedia.org/wikipedia/en/3/3f/Schulich.png",
+						link: "http://schulich.ucalgary.ca"
+					},
+					{
+						name: "University of Calgary",
+						img:
+							"https://symbiont2018.files.wordpress.com/2018/10/university-of-calgary-logo-transparent-uc-horz-rgb-11.png?w=1400",
+						link: "https://www.ucalgary.ca/"
+					}
+				]
 			}
 		]
 	};
@@ -32,31 +41,46 @@ class Sponsors extends Component {
 		return (
 			<section id="sponsors">
 				<SectionTitle title="Sponsors" />
-				<div className="text-center">
-					<h2>Gold</h2>
-					<br />
-					{this.renderSponsors("gold", 150)}
-					<br />
-					<br />
-					<h2>Silver</h2>
-					<br />
-					{this.renderSponsors("silver")}
-					<br />
-					<br />
-				</div>
+				{this.renderSponsorLevels(this.state.sponsorLevels)}
 			</section>
 		);
 	}
 
-	renderSponsors(tier, height = 100) {
-		const { sponsors } = this.state;
-		if (sponsors.length === 0) {
-			return <h5 className="text-muted">Become a sponsor today!</h5>;
-		}
+	// renderSponsors(tier, height = 100) {
+	// 	const { sponsors } = this.state;
+	// 	if (sponsors.length === 0) {
+	// 		return <h5 className="text-muted">Become a sponsor today!</h5>;
+	// 	}
 
-		return this.state.sponsors
-			.filter(s => s.tier === tier)
-			.map(s => <Sponsor sponsor={s} height={height} />);
+	// 	return this.state.sponsors.map(s => (
+	// 		<Sponsor sponsor={s} height={imgHeight} />
+	// 	));
+	// }
+
+	renderSponsorLevels(sponsorLevels) {
+		return sponsorLevels.map(sponsorLevel => {
+			const { level, imgHeight, sponsors } = sponsorLevel;
+
+			return (
+				<div key={level} className="mb-3">
+					<h3 className="mb-3">{level}</h3>
+					<div className="row justify-content-md-center">
+						{sponsors.map(sponsor =>
+							this.renderSponsor(sponsor, imgHeight)
+						)}
+					</div>
+				</div>
+			);
+		});
+	}
+
+	renderSponsor(sponsor, imgHeight) {
+		const { name, img, link } = sponsor;
+		return (
+			<div key={name} className="col-md-4">
+				<Sponsor img={img} link={link} imgHeight={imgHeight} />
+			</div>
+		);
 	}
 }
 

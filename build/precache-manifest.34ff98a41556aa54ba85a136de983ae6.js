1583ec914490783a3400f8b6a0f1b4c4dfe885a7
R Lee <alpha.r.lee.dev@gmail.com>
2019-11-01 22:46:45 -0600

    Added build version

---
+++ build/precache-manifest.34ff98a41556aa54ba85a136de983ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ddb776a21a78880e0c486ae51a495e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf389cf01238f397c2e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cdf389cf01238f397c2e",
+    "url": "/static/js/2.906d8a53.chunk.js"
+  },
+  {
+    "revision": "484d308982f0a71cde4d",
+    "url": "/static/js/main.05b99342.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
